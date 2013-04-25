ed803bb7a94316bf1b26dfa25ccffc37ce65f8b2
cymcsg <cymcsg@gmai.com>
2013-04-25 08:45:31 +0800

    init0.01

--- QdNewsProject1/src/com/fss/qdnewsproject1/Constant/Contant1.java
+++ QdNewsProject1/src/com/fss/qdnewsproject1/Constant/Contant1.java
@@ -1,6 +1,6 @@
 package com.fss.qdnewsproject1.Constant;
 
 public class Contant1 {
-	public static String jsonUrlString="http://192.168.1.7:8090/JsonTest1/servlet/JsonServlet";
+	public static String jsonUrlString="http://192.168.1.4:8090/JsonTest1/servlet/JsonServlet";
 
 }
